0b0033cf689f925d85218db531ff42ceab3e13e1
Charles Swaleh <charlswall@live.com>
2023-07-25 20:54:56 +0300

    added the validations to the license model

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,2 +1,5 @@
 class License < ApplicationRecord
+  validates :license_name, :purchase_date,  :expiry_date, :number_of_users, presence: true
+  validates :number_of_users, numericality: { only_integer: true }
+  validates :expiry_date, comparison: { greater_than: :purchase_date }
 end
