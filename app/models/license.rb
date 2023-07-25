9a991bcd88fbaa59bd6433de1ccf528a772cc492
Charles Swaleh <charlswall@live.com>
2023-07-25 21:19:45 +0300

    fix validation errors where user can buy a license in the future with 0 number of users

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,5 +1,7 @@
 class License < ApplicationRecord
   validates :license_name, :purchase_date,  :expiry_date, :number_of_users, presence: true
   validates :number_of_users, numericality: { only_integer: true }
+  validates :number_of_users, comparison: { greater_than_or_equal_to: 1 }
+  validates :purchase_date, comparison: {less_than_or_equal_to: Date.today}
   validates :expiry_date, comparison: { greater_than: :purchase_date }
 end
