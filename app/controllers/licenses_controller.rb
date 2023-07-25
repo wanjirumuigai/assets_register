8d4002271b395d2b2903bad3db27aced266e0429
Charles Swaleh <charlswall@live.com>
2023-07-25 21:08:45 +0300

    created the create route for the licesnse

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,5 +1,6 @@
 class LicensesController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :license_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_params
 
   def index
     licenses = License.all
@@ -11,6 +12,11 @@ rescue_from ActiveRecord::RecordNotFound, with: :license_not_found
     render json: license, status: :ok
   end
 
+  def create
+    new_license = License.create!(license_params)
+    render json: new_license, status: :created
+  end
+
 
   private
 
@@ -20,6 +26,14 @@ rescue_from ActiveRecord::RecordNotFound, with: :license_not_found
 
   def license_not_found
     render json: {error: "License not found"}, status: :not_found
+  end
+
+  def invalid_params(invalid)
+    render json: {error: invalid.record.errors}, status: :unprocessable_entity
+  end
+
+  def license_params
+    params.permit(:license_name, :purchase_date, :expiry_date, :number_of_users)
   end
 
 end
