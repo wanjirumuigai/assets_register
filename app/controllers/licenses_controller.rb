af663f94c4b98eb940acd80bf8019c48b443d0a2
Charles Swaleh <charlswall@live.com>
2023-07-25 20:31:58 +0300

    added the show route for licenses

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,8 +1,25 @@
 class LicensesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :license_not_found
 
   def index
     licenses = License.all
     render json: licenses, status: :ok
+  end
+
+  def show
+    license = find_license()
+    render json: license, status: :ok
+  end
+
+
+  private
+
+  def find_license()
+    License.find(params[:id])
+  end
+
+  def license_not_found
+    render json: {error: "License not found"}, status: :not_found
   end
 
 end
