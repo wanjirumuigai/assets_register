31ae439e2ba88ddefed27e521d3ce0bf62072ed8
Pauline Muigai <wanjirumuigai@gmail.com>
2023-07-27 14:17:43 +0300

    created users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :assigns
-  resources :users
+  resources :users, only: [:index, :show, :update, :create]
   resources :licenses, only: [:index, :show, :update, :create]
   resources :assets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post '/login', to: 'auth#create'
-  post '/users/create', to: 'users#create'
+
 end
