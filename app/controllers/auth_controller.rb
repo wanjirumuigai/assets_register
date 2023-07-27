a070e1329bb1d4df5e3bb4692a0fd1bf98151a38
Pauline Muigai <wanjirumuigai@gmail.com>
2023-07-27 08:45:55 +0300

    fixed login error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,18 +2,18 @@ class AuthController < ApplicationController
   skip_before_action :authorized, only: :create
 
   def create
-     user = User.find_by(email: params[:email])
-     if user&.authenticate(params[:password])
-       token = encode_token({user_id: user.id})
-       render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
-     else
-      render json: {error: "Invalid email or password"}, status: :unauthorized
-     end
-  end
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      token = encode_token({user_id: user.id})
+      render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
+    else
+     render json: {error: "Invalid email or password"}, status: :unauthorized
+    end
+ end
 
-  private
+ private
 
-  def login_params
-    params.permit(:email, :password)
-  end
+ def login_params
+   params.permit(:email, :password)
+ end
 end
