be02fcc9a4ab849e84c4a8e60f8d1b02873e094d
Charles Swaleh <charlswall@live.com>
2023-07-30 23:31:43 +0300

    created the index route to show all assets

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,2 +1,19 @@
 class AssetsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :asset_not_found
+skip_before_action :authorized
+
+  def index
+    assets = Asset.all
+    render json: assets, status: :ok
+  end
+
+  private
+
+  def find_asset
+    Asset.find(params[:id])
+  end
+
+  def asset_not_found
+    render json: {error: "Asset not found"}, status: :not_found
+  end
 end
