842ca56aad6b62ab84a1e2bf61abc4a9e9ea50ad
Charles Swaleh <charlswall@live.com>
2023-07-30 23:21:20 +0300

    defined the routes for the assets model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :assigns
   resources :users, only: [:index, :show, :update, :create]
   resources :licenses, only: [:index, :show, :update, :create]
-  resources :assets
+  resources :assets, only: [:index, :show, :update, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
