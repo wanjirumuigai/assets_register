28711ae3568502a22c3bcab573cb8b3e9ea56dee
Pauline Muigai <wanjirumuigai@gmail.com>
2023-07-31 15:55:51 +0300

    created assign routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :assets
+
   validates :firstname, :lastname, :email, :password, :role, :designation, :department, presence: true
   validates :role, inclusion: {in: %w(super_admin Admin User), message: "%{value} is not a valid role type. Input either super_admin, admin or user"}
 end
