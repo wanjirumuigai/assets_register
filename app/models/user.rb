8f66a91cd36418c034657dce5cbb007c34c330a0
Pauline Muigai <wanjirumuigai@gmail.com>
2023-08-01 15:45:41 +0300

    change order of seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :firstname, :lastname, :email, :password, :role, :designation, :department, presence: true
-  validates :role, inclusion: {in: %w(super_admin admin user), message: "%{value} is not a valid role type. Input either super_admin, admin or user"}
+  validates :role, inclusion: {in: %w(super_admin Admin User), message: "%{value} is not a valid role type. Input either super_admin, admin or user"}
 
   has_many :assigns
 
